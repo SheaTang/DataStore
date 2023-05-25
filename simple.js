8073bcaba9ac1826d6318c282ac9eedd63293593
Sheena Bish Tang <101611557+SheaTang@users.noreply.github.com>
2023-05-25 04:57:50 -0500

    https://www.npmjs.com/package/lowdb?activeTab=code

--- simple.js
+++ simple.js
@@ -1,13 +1,15 @@
-var low     = require('lowdb');
-var fs      = require('lowdb/adapters/FileSync');
-var adapter = new fs('db.json');
-var db      = low(adapter);
+var low = require("lowdb");
+var fs = require("lowdb/adapters/FileSync");
+var adapter = new fs("db.json");
+var db = low(adapter);
 
 // init the data store
 // ---------------------------
-db.defaults({ posts: []}).write();
- 
-console.log(db.get('posts').value());
+db.defaults({ posts: [], user: {} }).write();
+
+// Add a post
+db.get("posts").push({ id: 1, title: "lowdb is awsome" }).write();
+console.log(db.get("posts").value());
 
 // count posts
 // ----------------------------
