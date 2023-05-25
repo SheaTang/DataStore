8962e195f821641a818a1b301a9ee8acb61273a2
Sheena Bish Tang <101611557+SheaTang@users.noreply.github.com>
2023-05-25 09:49:06 -0500

    Find all post IDs (plus clean-up)

--- simple.js
+++ simple.js
@@ -13,10 +13,18 @@ console.log(db.get("posts").value());
 // count posts
 // ----------------------------
 // YOUR CODE
-
+db.get("posts").push({ id: 1, title: "post 1", published: true }).write();
+db.get("posts").push({ id: 2, title: "post 2", published: false }).write();
+db.get("posts").push({ id: 3, title: "post 3", published: true }).write();
+db.get("posts").push({ id: 4, title: "post 4", published: false }).write();
 // find all posts ids
 // ----------------------------
 // YOUR CODE
+let allPosts = db
+  .get("posts")
+  .value()
+  .map((post) => post.id);
+console.log("allPosts", allPosts);
 
 // all matches of published false
 // ----------------------------
